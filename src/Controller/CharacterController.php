769241e4c38539220054633b8f4179d61e419dbf
Mathias Schreiber <mathias.schreiber@typo3.com>
2019-09-05 09:27:43 +0200

    [FEATURE] Added Loots to character view

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Attendance;
+use App\Entity\Character;
+use App\Entity\Raid;
 use App\Repository\CharacterRepository;
+use App\Repository\LootRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +17,15 @@ class CharacterController extends AbstractController
      * @var CharacterRepository
      */
     private $characterRepository;
+    /**
+     * @var LootRepository
+     */
+    private $lootRepository;
 
-    public function __construct(CharacterRepository $characterRepository)
+    public function __construct(CharacterRepository $characterRepository, LootRepository $lootRepository)
     {
         $this->characterRepository = $characterRepository;
+        $this->lootRepository = $lootRepository;
     }
 
     /**
@@ -26,9 +35,38 @@ class CharacterController extends AbstractController
      */
     public function index(int $charId): Response
     {
+        $raids = [];
         $char = $this->characterRepository->find($charId);
+        if (!$char) {
+            return new Response('Char not found', 404);
+        }
+        foreach ($char->getAttendances() as $attendance) {
+            $raids[] = $this->compileRaidData($attendance);
+        }
         return $this->render('character/index.html.twig', [
             'character' => $char,
+            'raids' => $raids
         ]);
+    }
+
+    private function findLootsPerRaidAndChar(Raid $raid, Character $character): array
+    {
+        $lootsPerRaid = $this->lootRepository->findBy([
+            'raid' => $raid,
+            'player' => $character
+        ]);
+        return $lootsPerRaid;
+    }
+
+    private function compileRaidData(Attendance $attendance): array
+    {
+        $output = [];
+        if ($attendance->getRaidnight()) {
+            $output = [
+                'raid' => $attendance->getRaidnight(),
+                'loots' => $this->findLootsPerRaidAndChar($attendance->getRaidnight(), $attendance->getPlayer())
+            ];
+        }
+        return $output;
     }
 }
