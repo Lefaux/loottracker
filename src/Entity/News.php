6ef747579c125bb7c9640892af7177c592d69601
Mathias Schreiber <mathias.schreiber@typo3.com>
2019-10-25 09:51:35 +0200

    [FEATURE] Added Newsfeed models

---
+++ src/Entity/News.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\NewsRepository")
+ */
+class News
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $publishedOn;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\NewsCategory", inversedBy="news")
+     */
+    private $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPublishedOn(): ?\DateTimeInterface
+    {
+        return $this->publishedOn;
+    }
+
+    public function setPublishedOn(\DateTimeInterface $publishedOn): self
+    {
+        $this->publishedOn = $publishedOn;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCategory(): ?NewsCategory
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?NewsCategory $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
