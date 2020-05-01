7ecbc4b85eb0d426c7945f1dd66a68cbc99f84b5
Mathias Schreiber <mathias.schreiber@typo3.com>
2020-05-01 13:15:27 +0200

    [BUGFIX] Add Guild Rank Updater

--- src/Controller/GuildController.php
+++ src/Controller/GuildController.php
@@ -61,13 +61,53 @@ class GuildController extends AbstractController
         /** @var User $user */
         $user = $this->getUser();
         if ($user && $user->hasRole('ROLE_RAIDMANAGER')) {
+            $usedRanks = [];
+            $rankMapping = [
+                'Guildleader' => [
+                    'rank' => $this->rankRepository->findOneBy(['name' => 'Guildleader']),
+                    'twink' => false
+                ],
+                'Offizier' => [
+                    'rank' => $this->rankRepository->findOneBy(['name' => 'Officer']),
+                    'twink' => false
+                ],
+                'Offizierstwink' => [
+                    'rank' => $this->rankRepository->findOneBy(['name' => 'Officer']),
+                    'twink' => true
+                ],
+                'Core' => [
+                    'rank' => $this->rankRepository->findOneBy(['name' => 'Core']),
+                    'twink' => false
+                ],
+                'Raider' => [
+                    'rank' => $this->rankRepository->findOneBy(['name' => 'Raider']),
+                    'twink' => false
+                ],
+                'Raid-Twink' => [
+                    'rank' => $this->rankRepository->findOneBy(['name' => 'Raider']),
+                    'twink' => true
+                ],
+                'Family&Friends' => [
+                    'rank' => $this->rankRepository->findOneBy(['name' => 'Family&Friends']),
+                    'twink' => false
+                ],
+                'Twink' => [
+                    'rank' => $this->rankRepository->findOneBy(['name' => 'Raider']),
+                    'twink' => true
+                ],
+                'Trial' => [
+                    'rank' => $this->rankRepository->findOneBy(['name' => 'Trial']),
+                    'twink' => false
+                ],
+            ];
             $guildString = $request->request->get('guilddata');
             $parser = new LuaParser($guildString);
             $guildData = $parser->parse();
             if (array_key_exists('gbm_guildmembers', $guildData)) {
                 foreach ($guildData['gbm_guildmembers'] as $memberRow) {
                     $member = explode(' | ', $memberRow);
-                    $this->insertOrUpdateChar($member);
+                    $usedRanks[$member['3']] = 1;
+                    $this->insertOrUpdateChar($member, $rankMapping);
                 }
             }
             $this->entityManager->flush();
@@ -76,31 +116,29 @@ class GuildController extends AbstractController
         return $this->redirectToRoute('guild');
     }
 
-    private function insertOrUpdateChar(array $char): void
+    private function insertOrUpdateChar(array $char, array $rankMapping): void
     {
         $charName = str_replace('-Razorfen', '', $char[0]);
-        if ((int)$char[1] < 55) {
+        if (!array_key_exists($char[3], $rankMapping)) {
+            $this->addFlash('danger', 'Rank "' . $char[3]  . '" not found in mapping for "' .  $charName.'"');
             return;
         }
         $character = $this->characterRepository->findOneBy(['name' => $charName]);
-        if (!$character) {
-            $character = new Character();
-            $this->addFlash('success', 'Added '. $charName);
-            $character->setName($charName);
-        }
-        $character->setTwink(false);
-        if ($char[3] === 'Twink') {
-            $character->setTwink(true);
-        }
-        $rank = $this->rankRepository->findOneBy(['name' => $char[3]]);
-        if ($rank) {
+        if ($character) {
+            $character->setTwink(false);
+            $rank = $rankMapping[$char[3]]['rank'];
+            if (!$rank) {
+                $this->addFlash('danger', 'Rank "' . $char[3]  . '" not found in database for "' .  $charName.'"');
+                return;
+            }
             $character->setRank($rank);
+            $character->setTwink($rankMapping[$char[3]]['twink']);
+            $upperClass = strtoupper($char[2]);
+            $character->setClass(constant("\App\Utility\WowClassUtility::CLASS_$upperClass"));
+            if ($character->getNote() === null) {
+                $character->setNote($char[4]);
+            }
+            $this->entityManager->persist($character);
         }
-        $upperClass = strtoupper($char[2]);
-        $character->setClass(constant("\App\Utility\WowClassUtility::CLASS_$upperClass"));
-        if ($character->getNote() === null) {
-            $character->setNote($char[4]);
-        }
-        $this->entityManager->persist($character);
     }
 }
